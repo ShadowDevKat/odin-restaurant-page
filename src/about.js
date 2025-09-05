7aaddd623be9cbd458d3c145244c6abb7e3f49d6
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-09-05 06:35:36 +0500

    Added content and styles for about us page

--- src/about.js
+++ src/about.js
@@ -1,15 +1,23 @@
 import { setBackgroundClass, contentDiv } from ".";
 
+const aboutContent = {
+    heading: "About Us",
+    mainText: "At Crimson Flame, we're more than a steakhouse — we're a celebration of fire, flavor, and craftsmanship. From hand-selected cuts to sides made with fresh ingredients, every dish is crafted with passion and served with warmth. Here, great food meets unforgettable moments."
+}
+
 export function displayAboutUs() {
     setBackgroundClass("about-bg");
 
     const containerDiv = document.createElement("div");
-    containerDiv.classList.add("about");
+    containerDiv.classList.add("about", "glass-effect");
 
     const heading = document.createElement("h1");
-    heading.textContent = "About";
+    heading.classList.add("about-title");
+    heading.textContent = aboutContent.heading;
+
     const para = document.createElement("p");
-    para.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dignissimos, esse, quas, ut ipsa atque velit nostrum aliquid eius sapiente nihil quae tempore error ? Hic cupiditate doloribus ipsa exercitationem nemo.";
+    para.classList.add("about-text");
+    para.textContent = aboutContent.mainText;
 
     containerDiv.append(heading, para);
     contentDiv.appendChild(containerDiv);
